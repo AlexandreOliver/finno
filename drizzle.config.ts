20eeb24f8cc932ddab2c6a2e2f537d3a43d0d035
AlexandreOliver <alexandreferreiradeoliveira40@gmail.com>
2026-04-24 20:32:06 -0300

    chore: adiciona ORM drizzle e configura

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "postgresql",
+  out: "infra/database/migrations",
+  schema: "infra/database/schemas",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+    ssl: process.env.NODE_ENV === "production",
+  },
+  casing: "snake_case",
+});
