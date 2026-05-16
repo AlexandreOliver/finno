382737da5946c12416e7c0ddaba0de4de514e659
AlexandreOliver <alexandreferreiradeoliveira40@gmail.com>
2026-05-15 23:00:43 -0300

    feat(NavBar): adiciona o hook usePathname

--- features/dasboard/components/NavBar.tsx
+++ features/dasboard/components/NavBar.tsx
@@ -1,16 +1,33 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { BanknoteArrowDown, BanknoteArrowUp, Home, Wallet } from "lucide-react";
 import { Separator } from "@base-ui/react";
+import clsx from "clsx";
 
 export function NavBar() {
+  const pathname = usePathname();
+
   return (
     <header className="pt-2 flex justify-center">
       <nav className="flex gap-1 text-md rounded-t-md bg-card px-2 py-1">
-        <div className="flex items-center gap-2">
-          <Link href="#" prefetch={false}>
-            <div className="flex rounded-md justify-center items-center gap-2 px-1 py-2 hover:bg-gray-300 hover:text-black">
+        <div className="flex items-center gap-2 ">
+          <Link
+            href="#"
+            prefetch={false}
+            className={clsx(
+              pathname === "/dashboard" &&
+                "border-b-2 border-blue-600 dark:border-blue-400",
+            )}
+          >
+            <div
+              className={`
+                flex justify-center items-center gap-2
+                rounded-md px-2 py-2
+                hover:bg-gray-300/20 hover:text-black dark:hover:text-gray-300
+                `}
+            >
               <Home size={20} />
               <span>Home</span>
             </div>
@@ -21,8 +38,21 @@ export function NavBar() {
           />
         </div>
         <div className="flex items-center gap-2">
-          <Link href="#" prefetch={false}>
-            <div className="flex rounded-md justify-center items-center gap-2 px-1 py-2 hover:bg-gray-300 hover:text-black">
+          <Link
+            href="#"
+            prefetch={false}
+            className={clsx(
+              pathname === "/expenses" &&
+                "border-b-2 border-blue-600 dark:border-blue-400",
+            )}
+          >
+            <div
+              className={`
+                flex justify-center items-center gap-2
+                rounded-md px-2 py-2
+                hover:bg-gray-300/20 hover:text-black dark:hover:text-gray-300
+                `}
+            >
               <BanknoteArrowDown size={22} />
               <span>Gastos</span>
             </div>
@@ -33,8 +63,20 @@ export function NavBar() {
           />
         </div>
         <div className="flex items-center gap-2">
-          <Link href="#" prefetch={false}>
-            <div className="flex rounded-md justify-center items-center gap-2 px-1 py-2 hover:bg-gray-300 hover:text-black">
+          <Link
+            href="#"
+            prefetch={false}
+            className={clsx(
+              pathname === "/incomes" &&
+                "border-b-2 border-blue-600 dark:border-blue-400",
+            )}
+          >
+            <div
+              className={`
+                flex justify-center items-center gap-2
+                rounded-md px-2 py-2
+                hover:bg-gray-300/20 hover:text-black dark:hover:text-gray-300`}
+            >
               <BanknoteArrowUp size={22} />
               <span>Receita</span>
             </div>
@@ -44,12 +86,26 @@ export function NavBar() {
             className="h-6 border-r border-gray-300 dark:border-card-foreground/20 mr-1"
           />
         </div>
-        <Link href="#" prefetch={false}>
-          <div className="flex rounded-md justify-center items-center gap-2 px-1 py-2 hover:bg-gray-300 hover:text-black">
-            <Wallet size={20} />
-            <span>Carteiras</span>
-          </div>
-        </Link>
+        <div className="flex items-center gap-2">
+          <Link
+            href="#"
+            prefetch={false}
+            className={clsx(
+              pathname === "/wallets" &&
+                "border-b-2 border-blue-600 dark:border-blue-400",
+            )}
+          >
+            <div
+              className={`
+                flex justify-center items-center gap-2
+                rounded-md px-2 py-2
+                hover:bg-gray-300/20 hover:text-black dark:hover:text-gray-300`}
+            >
+              <Wallet size={20} />
+              <span>Carteiras</span>
+            </div>
+          </Link>
+        </div>
       </nav>
     </header>
   );
