470a1b9998b2c771876214fb7b6f632f2c44389a
AlexandreOliver <alexandreferreiradeoliveira40@gmail.com>
2026-05-18 14:53:43 -0300

    ui: muda as cores da aplicação

--- features/dashboard/components/HeaderDashboard.tsx
+++ features/dashboard/components/HeaderDashboard.tsx
@@ -12,7 +12,7 @@ export function HeaderDashboard() {
   const nome = formatNome(user?.firstName as string);
   const sobrenome = formatNome(user?.lastName as string);
   return (
-    <header className="border-gray-400 border-b bg-white dark:bg-background">
+    <header className="border-gray-400 border-b bg-background dark:bg-background">
       <div className="boxed p-2 flex justify-between">
         <div className="flex gap-3 justify-center items-center">
           <Coins size={33} className="text-black/60 dark:text-foreground" />
