6a1eeb1699419a398b73a2dc9e0a874a05376710
AlexandreOliver <alexandreferreiradeoliveira40@gmail.com>
2026-05-19 15:27:55 -0300

    feat(components): Cria o componente SelectField

---
+++ features/transactions/components/SelectField.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+interface SelectFieldProps {
+  isError: boolean;
+  placeholder: string;
+  nameForm: string;
+  data: {
+    id: string;
+    label: string;
+  }[];
+}
+
+export function SelectField(props: SelectFieldProps) {
+  return (
+    <Select
+      name={props.nameForm}
+      itemToStringValue={(item: (typeof props.data)[number]) =>
+        item ? item.id : ""
+      }
+      isItemEqualToValue={(itemValue, value) =>
+        !!itemValue && !!value && itemValue.id === value.id
+      }
+    >
+      <SelectTrigger className="w-35" aria-invalid={props.isError}>
+        <SelectValue placeholder={props.placeholder} />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {props.data.map((ctg) => (
+            <SelectItem key={ctg.id} value={ctg}>
+              {ctg.label}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
