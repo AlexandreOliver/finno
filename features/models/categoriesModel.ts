02b114f4ca15a026e5a4c4c793cf973c802fd9d6
AlexandreOliver <alexandreferreiradeoliveira40@gmail.com>
2026-05-19 15:06:42 -0300

    feat(models): Cria a model categoriesModel

---
+++ features/models/categoriesModel.ts
@@ -0,0 +1,61 @@
+import "server-only";
+import db from "@/infra/database";
+import { categories } from "@/infra/database/schemas/categories";
+import { createInsertSchema } from "drizzle-zod";
+import zod from "zod";
+import { eq, isNull, or } from "drizzle-orm";
+
+export const categorySchema = createInsertSchema(categories);
+
+type TypeCategoryCreate = Omit<
+  zod.infer<typeof categorySchema>,
+  "id" | "dueDate"
+>;
+
+async function create(movObject: TypeCategoryCreate) {
+  const newCategory = categorySchema.parse(movObject);
+
+  await db.insert(categories).values(newCategory);
+}
+
+type ColumnsTypes = typeof categories.$inferSelect;
+
+export type FunctionFindAll = <K extends keyof ColumnsTypes>({
+  userId,
+  returnFields,
+}: {
+  userId?: string;
+  returnFields: readonly K[];
+}) => Promise<Pick<ColumnsTypes, K>[]>;
+
+const findAll: FunctionFindAll = async ({ userId, returnFields }) => {
+  if (returnFields.length === 0) {
+    return [];
+  }
+
+  const selectCollumns = returnFields.reduce(
+    (acc, column) => {
+      acc[column] = categories[column];
+      return acc;
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    {} as any,
+  );
+
+  const categoriesList = await db
+    .select(selectCollumns)
+    .from(categories)
+    .where(or(isNull(categories.userId), eq(categories.userId, userId ?? "")));
+
+  return categoriesList as unknown as Pick<
+    ColumnsTypes,
+    (typeof returnFields)[number]
+  >[];
+};
+
+const movementsModel = {
+  create,
+  findAll,
+};
+
+export default movementsModel;
