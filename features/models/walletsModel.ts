aa9e368ff479c6d71b2570f16b1262f67d4424dc
AlexandreOliver <alexandreferreiradeoliveira40@gmail.com>
2026-05-19 15:21:26 -0300

    feat(models): Cria a model walletsModel

---
+++ features/models/walletsModel.ts
@@ -0,0 +1,49 @@
+import "server-only";
+
+import db from "@/infra/database";
+import { wallets } from "@/infra/database/schemas/wallets";
+import { eq, isNull, or } from "drizzle-orm";
+
+type ColumnsTypes = typeof wallets.$inferSelect;
+
+export type FunctionFindAllByOwner = <K extends keyof ColumnsTypes>({
+  ownerId,
+  returnFields,
+}: {
+  ownerId: string;
+  returnFields: readonly K[];
+}) => Promise<Pick<ColumnsTypes, K>[]>;
+
+const findAllByOwner: FunctionFindAllByOwner = async ({
+  returnFields,
+  ownerId,
+}) => {
+  if (returnFields.length === 0) {
+    return [];
+  }
+
+  const selectCollumns = returnFields.reduce(
+    (acc, column) => {
+      acc[column] = wallets[column];
+      return acc;
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    {} as any,
+  );
+
+  const walletsFromDb = await db
+    .select(selectCollumns)
+    .from(wallets)
+    .where(or(isNull(wallets.ownerId), eq(wallets.ownerId, ownerId ?? "")));
+
+  return walletsFromDb as unknown as Pick<
+    ColumnsTypes,
+    (typeof returnFields)[number]
+  >[];
+};
+
+const walletsModel = {
+  findAllByOwner,
+};
+
+export default walletsModel;
