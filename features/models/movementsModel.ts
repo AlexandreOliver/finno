23f4181e1a20079db0b1fbf4664780674ad7de37
AlexandreOliver <alexandreferreiradeoliveira40@gmail.com>
2026-05-22 22:14:02 -0300

    feat(models): adiciona metodos finds, delete e count
    - Adiciona tipos genericos para dar suporte aos métodos
    -Adiciona os métodos findByWalletForQuery,
    findByIdWithCategory e findByWalletIdWithCategory
    -Adiciona método deleteById

--- features/models/movementsModel.ts
+++ features/models/movementsModel.ts
@@ -3,6 +3,8 @@ import db from "@/infra/database";
 import { movements } from "@/infra/database/schemas/movements";
 import { createInsertSchema } from "drizzle-zod";
 import zod from "zod";
+import { and, between, eq, SQL, desc } from "drizzle-orm";
+import { categories } from "@/infra/database/schemas/categories";
 
 export const movementsSchema = createInsertSchema(movements, {
   amount: (schema) =>
@@ -25,6 +27,8 @@ export const movementsSchema = createInsertSchema(movements, {
   description: (schema) => schema.min(2, { error: "Descrição curta demais" }),
 });
 
+export type ColumnsTypes = typeof movements.$inferSelect;
+
 type TypeMovementsCreate = Omit<
   zod.infer<typeof movementsSchema>,
   "id" | "dueDate"
@@ -36,8 +40,175 @@ async function create(movObject: TypeMovementsCreate) {
   await db.insert(movements).values(newMovement);
 }
 
+export type QueryParamsMovements = {
+  limit?: number;
+  page?: number;
+  month?: number;
+  year?: number;
+};
+
+export type ArgsFindAllMovements<K extends keyof ColumnsTypes> = {
+  walletId: string;
+  returnFields: readonly K[];
+  query?: QueryParamsMovements;
+};
+
+export type FunctionFindAllMoviments = <K extends keyof ColumnsTypes>(
+  args: ArgsFindAllMovements<K>,
+) => Promise<Pick<ColumnsTypes, K>[]>;
+
+const findByWalletIdForQuery: FunctionFindAllMoviments = async ({
+  walletId,
+  returnFields,
+  query,
+}) => {
+  if (!walletId || returnFields.length === 0) return [];
+
+  const selectCollumns = returnFields.reduce(
+    (acc, column) => {
+      acc[column] = movements[column];
+      return acc;
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    {} as any,
+  );
+
+  const filters: SQL[] = [];
+
+  if (query?.month) {
+    const startDate = new Date(new Date().getFullYear(), query.month - 1, 1);
+    const endDate = new Date(new Date().getFullYear(), query.month, 0);
+    filters.push(between(movements.executedAt, startDate, endDate));
+  }
+  if (query?.year) {
+    const startDate = new Date(query.year, new Date().getMonth());
+    const endDate = new Date(query.year + 1, new Date().getMonth());
+    filters.push(between(movements.executedAt, startDate, endDate));
+  }
+
+  let consulta;
+  if (query?.limit) {
+    consulta = db.$with("sql").as(
+      db
+        .select(selectCollumns)
+        .from(movements)
+        .where(filters.length > 0 ? and(...filters) : undefined)
+        .orderBy(movements.executedAt, desc(movements.amount))
+        .offset(((query?.page ?? 1) - 1) * query.limit)
+        .limit(query.limit),
+    );
+  } else {
+    consulta = db.$with("sql").as(
+      db
+        .select(selectCollumns)
+        .from(movements)
+        .where(filters.length > 0 ? and(...filters) : undefined)
+        .orderBy(movements.executedAt, desc(movements.amount))
+        .offset(((query?.page ?? 1) - 1) * (query?.limit ?? 0)),
+    );
+  }
+  const movementsResult = await db.with(consulta).select().from(consulta);
+
+  return movementsResult as unknown as Pick<
+    ColumnsTypes,
+    (typeof returnFields)[number]
+  >[];
+};
+
+const deleteById = async (movementId: string) => {
+  const returnDb = await db
+    .delete(movements)
+    .where(eq(movements.id, movementId))
+    .returning({ id: movements.id });
+
+  return returnDb[0];
+};
+
+const count = async (walletId?: string) => {
+  let count;
+
+  if (walletId) {
+    count = await db.$count(movements, eq(movements.walletId, walletId));
+    return count;
+  }
+
+  count = await db.$count(movements);
+
+  return count;
+};
+
+const findByIdWithCategory = async <
+  K extends keyof Omit<ColumnsTypes, "categoryId">,
+>({
+  id,
+  returnFields,
+}: {
+  id: string;
+  returnFields: readonly K[];
+}) => {
+  if (!id || returnFields.length === 0) return null;
+
+  const selectCollumns = returnFields.reduce(
+    (acc, column) => {
+      acc[column] = movements[column];
+      return acc;
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    { labelCategory: categories.label, categoryId: categories.id } as any,
+  );
+
+  const category = await db
+    .select(selectCollumns)
+    .from(movements)
+    .where(eq(movements.id, id))
+    .leftJoin(categories, eq(categories.id, movements.categoryId));
+
+  return category[0] as unknown as Pick<
+    ColumnsTypes,
+    (typeof returnFields)[number]
+  >;
+};
+
+const findByWalletIdWithCategory = async <
+  K extends keyof Omit<ColumnsTypes, "categoryId">,
+>({
+  walletId,
+  returnFields,
+}: {
+  walletId: string;
+  returnFields: readonly K[];
+}) => {
+  if (!walletId || returnFields.length === 0) return null;
+
+  const selectCollumns = returnFields.reduce(
+    (acc, column) => {
+      acc[column] = movements[column];
+      return acc;
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    { labelCategory: categories.label, categoryId: categories.id } as any,
+  );
+
+  const category = await db
+    .select(selectCollumns)
+    .from(movements)
+    .where(eq(movements.walletId, walletId))
+    .orderBy(movements.executedAt, movements.amount)
+    .leftJoin(categories, eq(categories.id, movements.categoryId));
+
+  return category as unknown as Pick<
+    ColumnsTypes,
+    (typeof returnFields)[number]
+  >[];
+};
+
 const movementsModel = {
   create,
+  findByWalletIdForQuery,
+  deleteById,
+  count,
+  findByIdWithCategory,
+  findByWalletIdWithCategory,
 };
 
 export default movementsModel;
