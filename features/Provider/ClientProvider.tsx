630a75c9c0d66a0b166ad5fd7c35962ce95720e6
AlexandreOliver <alexandreferreiradeoliveira40@gmail.com>
2026-05-23 21:47:11 -0300

    feat(provider): cria o componente ClientProvider do tanstack

---
+++ features/Provider/ClientProvider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { ReactNode, useState } from "react";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export default function ClientProvider({ children }: { children: ReactNode }) {
+  const [QueryClientInstance] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: { queries: { staleTime: 1000 * 60 * 5 } },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={QueryClientInstance}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
