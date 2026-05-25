04c99256191d7c53f07d1cb13320a9b41def5bf6
AlexandreOliver <alexandreferreiradeoliveira40@gmail.com>
2026-05-25 15:12:25 -0300

    feat(dashboardlayout): adiciona um prefetchQuery para os dados de Wallets e hidrata a pagina

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,6 +5,14 @@ import { verifySession } from "@/features/authorization/services/verifysession";
 import { SessionProvider } from "@/features/authorization/contexts/SessionProvider";
 import { HeaderDashboard, NavBar } from "@/features/dashboard/components";
 
+import ClientProvider from "@/features/Provider/ClientProvider";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { findWallets } from "@/features/transactions/services/findWallets";
+
 export default async function DashboardLayout({
   children,
 }: Readonly<{
@@ -15,11 +23,28 @@ export default async function DashboardLayout({
   const authUser = await verifySession(sessionCookie?.value as string);
   if (!authUser.isAuth) redirect("/auth/signin");
 
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["wallets", { userId: authUser.user.id }],
+    queryFn: () =>
+      findWallets({
+        ownerId: authUser.user.id,
+        returnFields: ["id", "balance", "createdAt", "labelName", "updatedAt"],
+      }).then((d) => {
+        console.log("Prefetch feito");
+        return d;
+      }),
+  });
+
   return (
     <SessionProvider value={authUser}>
       <HeaderDashboard />
       <NavBar />
-      <main className="boxed">{children}</main>
+      <ClientProvider>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <main className="boxed">{children}</main>
+        </HydrationBoundary>
+      </ClientProvider>
     </SessionProvider>
   );
 }
