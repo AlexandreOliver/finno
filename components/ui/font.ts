c35420e77c9d7e42f503ca62da3e5e274fb54eca
AlexandreOliver <alexandreferreiradeoliveira40@gmail.com>
2026-05-26 18:07:03 -0300

    feat(fonts): adiciona novo peso 200 para a fonte Geist

--- components/ui/font.ts
+++ components/ui/font.ts
@@ -2,7 +2,7 @@ import { Geist } from "next/font/google";
 
 
 export const geist = Geist({
-  weight: ['300', '400', '500'],
+  weight: [ '200', '300', '400', '500'],
   subsets: ["latin"],
   variable: '--font-geist'
 });
