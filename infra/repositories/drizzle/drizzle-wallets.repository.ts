d98472c0494bcf7f4bce134e2348dadd813960be
AlexandreOliver <alexandreferreiradeoliveira40@gmail.com>
2026-06-24 18:00:59 -0300

    refactor(repositories): cria o repositorio drzzle de wallets

---
+++ infra/repositories/drizzle/drizzle-wallets.repository.ts
@@ -0,0 +1,57 @@
+import { IWalletsGateway } from "@/domain/wallets/wallets.gateway";
+import { Wallet } from "@/domain/wallets/wallets.entity";
+import { wallets } from "@/infra/database/schemas/wallets";
+import db from "@/infra/database";
+import { eq, and, SQL } from "drizzle-orm";
+
+export class WalletsRepositoryDrizzle implements IWalletsGateway {
+  private constructor(private readonly dbInstance: typeof db) {}
+
+  public static create(dbInstance: typeof db) {
+    return new WalletsRepositoryDrizzle(dbInstance);
+  }
+
+  public save: IWalletsGateway["save"] = async (props) => {
+    const result = await this.dbInstance
+      .insert(wallets)
+      .values(props.toJson())
+      .returning();
+
+    return !!result[0]?.id;
+  };
+
+  public async list(ownerId?: string) {
+    const filters: SQL[] = [];
+
+    if (ownerId) {
+      filters.push(eq(wallets.ownerId, ownerId));
+    }
+
+    const walletsDb = await this.dbInstance
+      .select()
+      .from(wallets)
+      .where(and(...filters));
+
+    return walletsDb.map((w) => Wallet.with(w));
+  }
+
+  public async findById(id: string) {
+    const result = await this.dbInstance
+      .select()
+      .from(wallets)
+      .where(eq(wallets.id, id));
+
+    if (!result || result.length === 0) return null;
+
+    return Wallet.with(result[0]);
+  }
+
+  public async deleteById(id: string) {
+    const result = await this.dbInstance
+      .delete(wallets)
+      .where(eq(wallets.id, id))
+      .returning();
+
+    return result.length > 0;
+  }
+}
