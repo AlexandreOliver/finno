e91a5174f880114131709761e34acddf7354cd9d
AlexandreOliver <alexandreferreiradeoliveira40@gmail.com>
2026-06-26 20:12:50 -0300

    feat(repositories): cria os repositorios drizzle users e session

---
+++ src/infra/repositories/drizzle/drizzle-sessions.repository.ts
@@ -0,0 +1,34 @@
+import { ISessionGateway } from "@/domain/session/session.gateway";
+import { sessions } from "@/infra/database/schemas/sessions";
+import db from "@/infra/database";
+
+export class SessionsRepositoryDrizzle implements ISessionGateway {
+  private constructor(private readonly dbInstance: typeof db) {}
+
+  public static create(dbInstance: typeof db) {
+    return new SessionsRepositoryDrizzle(dbInstance);
+  }
+
+  public save: ISessionGateway["save"] = async (session) => {
+    const sessionDto = session.toJson();
+
+    const result = await this.dbInstance
+      .insert(sessions)
+      .values(sessionDto)
+      .returning();
+
+    return result.length > 0;
+  };
+
+  public findActiveByToken: ISessionGateway["findActiveByToken"] = async () => {
+    throw new Error("Nao implementado");
+  };
+
+  public renew: ISessionGateway["renew"] = async () => {
+    throw new Error("Nao implementado");
+  };
+
+  public isActive: ISessionGateway["isActive"] = async () => {
+    throw new Error("Nao implementado");
+  };
+}
