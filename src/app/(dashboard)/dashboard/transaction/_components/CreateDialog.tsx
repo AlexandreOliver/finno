57ba386db2e9b4d887e92689e82c3ae486345658
AlexandreOliver <alexandreferreiradeoliveira40@gmail.com>
2026-07-18 21:34:38 -0300

    fix(createdialog): corrige erro de scroll no formulario de criação de movimentação

--- src/app/(dashboard)/dashboard/transaction/_components/CreateDialog.tsx
+++ src/app/(dashboard)/dashboard/transaction/_components/CreateDialog.tsx
@@ -30,41 +30,18 @@ interface CreateFormProps {
 
 export function CreateDialog(props: CreateFormProps) {
   const [open, setOpen] = React.useState(false);
-  const isDesktop = useMediaQuery("(min-width: 768px)");
 
-  if (isDesktop) {
-    return (
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger
-          render={<ButtonCreate variant={props.type} label={props.label} />}
-        />
-        <DialogContent className="sm:max-w-106.25">
-          <DialogHeader>
-            <DialogTitle>Nova Transação</DialogTitle>
-          </DialogHeader>
-          <CreateForm variant={props.type} />
-        </DialogContent>
-      </Dialog>
-    );
-  }
   return (
-    <Drawer open={open} onOpenChange={setOpen} modal={false}>
-      <DrawerTrigger asChild>
-        <ButtonCreate variant={props.type} label={props.label} />
-      </DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader className="text-left">
-          <DrawerTitle>{props.label}</DrawerTitle>
-        </DrawerHeader>
-        <div className="px-2">
-          <CreateForm variant={props.type} />
-        </div>
-        <DrawerFooter className="pt-2">
-          <DrawerClose asChild>
-            <Button variant="outline">Cancelar</Button>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger
+        render={<ButtonCreate variant={props.type} label={props.label} />}
+      />
+      <DialogContent className="sm:max-w-106.25">
+        <DialogHeader>
+          <DialogTitle>Nova Transação</DialogTitle>
+        </DialogHeader>
+        <CreateForm variant={props.type} />
+      </DialogContent>
+    </Dialog>
   );
 }
